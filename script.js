af6b5bb6be37765887b1f9abe686e70c87e17de6
aadik6 <aadarshkumarman@gmail.com>
2025-02-20 08:07:14 +0545

    first

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("signupForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  let name = document.getElementById(" add").value;
+  console.log("name", name);
+});
