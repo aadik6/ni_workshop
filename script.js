634d6230fd48fb3bd1891144c67e68d26e53f596
aadik6 <aadarshkumarman@gmail.com>
2025-02-21 08:57:53 +0545

    Day 3

--- script.js
+++ script.js
@@ -1,5 +1,43 @@
-document.getElementById("signupForm").addEventListener("submit", function (e) {
+document.getElementById("signupForm")?.addEventListener("submit", function (e) {
   e.preventDefault();
-  let name = document.getElementById(" add").value;
-  console.log("name", name);
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let password = document.getElementById("password").value;
+
+  const user = {
+    name,
+    email,
+    password,
+  };
+    console.log(user,"user")
+
+  localStorage.setItem("user",JSON.stringify(user))
 });
+console.log(localStorage.getItem("user"),"user") 
+
+
+
+document.getElementById("loginForm").addEventListener("submit", function(e){
+  e.preventDefault()
+  let email = document.getElementById("email").value
+  let password = document.getElementById("password").value
+  console.log("Email", email)
+  console.log("Password", password)
+
+  let user = JSON.parse(localStorage.getItem("user"))
+
+  if(!user){
+    alert("User not registered please sign up first");
+    window.location.href = '/'
+    return;
+  }
+
+  if(user.email == email && user.password == password){
+    console.log("Success")
+    window.location.href = '/home.html'
+  }else{
+    window.alert("Wrong credentials")
+  }
+  
+
+})
